6c8faaf39954dd49c117f32662db99990377370e
Overmind-PC\Overmind <vetalka84@gmail.com>
2017-09-05 22:21:23 +0300

    timer(lesson1)

--- www/src/app/app.module.ts
+++ www/src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PomodoroTimerComponent } from './pomodoro-timer';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    PomodoroTimerComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent,PomodoroTimerComponent ]
 })
 export class AppModule { }
